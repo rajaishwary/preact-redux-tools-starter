0b397ca53e986b46d7fcd8419212b3fe4f2af212
rajaishwary <rajaishwary@gmail.com>
2019-11-19 21:09:58 +0530

    update global styles with styled component

--- src/components/app/styles.js
+++ src/components/app/styles.js
@@ -1,5 +1,8 @@
-html, body {
-	height: 100%;
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html, body {
+    height: 100%;
 	width: 100%;
 	padding: 0;
 	margin: 0;
@@ -9,12 +12,12 @@ html, body {
 	color: #444;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-}
-
-* {
+  }
+  * {
 	box-sizing: border-box;
-}
+  }
+`;
 
-#app {
+export const AppContainer = styled.div`
 	height: 100%;
-}
+`;
